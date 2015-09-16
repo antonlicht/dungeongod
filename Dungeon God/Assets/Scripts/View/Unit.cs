6ed02740dc871efb34b592e3045460c22abc1689
Anton Licht <anton.licht@gmail.com>
2015-09-16 18:24:28 +0200

    setup dungeon

--- Dungeon God/Assets/Scripts/View/Unit.cs
+++ Dungeon God/Assets/Scripts/View/Unit.cs
@@ -3,4 +3,22 @@
 public class Unit : MonoBehaviour
 {
     public bool Draggable;
+
+    private Vector2 _position;
+
+    void Awake()
+    {
+        _position = transform.position;
+    }
+
+    public void Move(Vector2 newPos)
+    {
+        _position = newPos;
+    }
+
+    void FixedUpdate()
+    {
+        var rigidBody = GetComponent<Rigidbody2D>();
+        rigidBody.MovePosition(_position);
+    }
 }
--- Dungeon God/Assets/Scripts/View/UnitController.cs
+++ Dungeon God/Assets/Scripts/View/UnitController.cs
@@ -5,31 +5,38 @@ using System.Linq;
 public class UnitController : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
     private Unit _activeUnit;
-    private Vector3 _lastPos;
+    private Vector2 _offset;
     public void OnBeginDrag(PointerEventData eventData)
     {
-        var worldPos = Camera.main.ScreenToWorldPoint(eventData.position);
-        var hits = Physics2D.CircleCastAll(worldPos, .5f, Vector2.zero,100);
-        var hit = hits.FirstOrDefault(h => h.collider.GetComponent<Unit>() != null && h.collider.GetComponent<Unit>().Draggable);
+        var ray = Camera.main.ScreenPointToRay(eventData.position);
+        var hit = Physics2D.GetRayIntersectionAll(ray).FirstOrDefault(h => h.collider.GetComponent<Unit>() != null && h.collider.GetComponent<Unit>().Draggable);
         if(hit != default(RaycastHit2D))
         {
+            Debug.Log(hit.collider.name);
             _activeUnit = hit.collider.GetComponent<Unit>();
-            _lastPos = worldPos;
+            _activeUnit.GetComponent<Rigidbody2D>().isKinematic = false;
+            _offset = (Vector2)_activeUnit.transform.position - hit.point;
         }
     }
 
     public void OnDrag(PointerEventData eventData)
     {
+        int layerMask = LayerMask.GetMask("BackgroundCollider");
         if (_activeUnit)
         {
-            var worldPos = Camera.main.ScreenToWorldPoint(eventData.position);
-            _activeUnit.transform.Translate(worldPos - _lastPos);
-            _lastPos = worldPos;
+            var ray = Camera.main.ScreenPointToRay(eventData.position);
+            var hit = Physics2D.GetRayIntersection(ray, 1000f, layerMask);
+            if (hit)
+            {
+                _activeUnit.Move(hit.point + _offset);
+            }
+            
         }
     }
 
     public void OnEndDrag(PointerEventData eventData)
     {
+        _activeUnit.GetComponent<Rigidbody2D>().isKinematic = true;
         _activeUnit = null;
     }
 }
