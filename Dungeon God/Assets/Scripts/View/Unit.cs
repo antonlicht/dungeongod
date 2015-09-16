c519900a4356a62c85a9aae5436f7e5c5d9b7b60
Anton Licht <anton.licht@gmail.com>
2015-09-16 18:39:41 +0200

    fix jumping through walls

--- Dungeon God/Assets/Scripts/View/Unit.cs
+++ Dungeon God/Assets/Scripts/View/Unit.cs
@@ -4,21 +4,67 @@ public class Unit : MonoBehaviour
 {
     public bool Draggable;
 
+    private Rigidbody2D _rigidbody;
     private Vector2 _position;
+    private bool _dragging;
+
+    public bool Dragging
+    {
+        get
+        {
+            return _dragging;
+        }
+        set
+        {
+            if (Draggable)
+            {
+                _dragging = value;
+                if (_dragging)
+                {
+                    Rigidbody.isKinematic = false;
+                }
+            }
+        }
+    }
+
+    public Rigidbody2D Rigidbody
+    {
+        get
+        {
+            if (_rigidbody == null)
+            {
+                _rigidbody = GetComponent<Rigidbody2D>();
+            }
+            return _rigidbody;
+        }
+    }
+
+
 
     void Awake()
     {
         _position = transform.position;
     }
 
-    public void Move(Vector2 newPos)
-    {
-        _position = newPos;
-    }
-
     void FixedUpdate()
     {
         var rigidBody = GetComponent<Rigidbody2D>();
         rigidBody.MovePosition(_position);
+        if (!Dragging)
+        {
+            _position = transform.position;
+            rigidBody.MovePosition(_position);        
+            Rigidbody.isKinematic = true;
+        }
     }
+
+    public void Move(Vector2 newPos)
+    {
+        if (Dragging)
+        {
+            _position = newPos;
+        }
+    }
+
+
 }
--- Dungeon God/Assets/Scripts/View/UnitController.cs
+++ Dungeon God/Assets/Scripts/View/UnitController.cs
@@ -12,18 +12,17 @@ public class UnitController : MonoBehaviour, IBeginDragHandler, IDragHandler, IE
         var hit = Physics2D.GetRayIntersectionAll(ray).FirstOrDefault(h => h.collider.GetComponent<Unit>() != null && h.collider.GetComponent<Unit>().Draggable);
         if(hit != default(RaycastHit2D))
         {
-            Debug.Log(hit.collider.name);
             _activeUnit = hit.collider.GetComponent<Unit>();
-            _activeUnit.GetComponent<Rigidbody2D>().isKinematic = false;
+            _activeUnit.Dragging = true;
             _offset = (Vector2)_activeUnit.transform.position - hit.point;
         }
     }
 
     public void OnDrag(PointerEventData eventData)
-    {
-        int layerMask = LayerMask.GetMask("BackgroundCollider");
+    {        
         if (_activeUnit)
         {
+            int layerMask = LayerMask.GetMask("BackgroundCollider");
             var ray = Camera.main.ScreenPointToRay(eventData.position);
             var hit = Physics2D.GetRayIntersection(ray, 1000f, layerMask);
             if (hit)
@@ -36,7 +35,10 @@ public class UnitController : MonoBehaviour, IBeginDragHandler, IDragHandler, IE
 
     public void OnEndDrag(PointerEventData eventData)
     {
-        _activeUnit.GetComponent<Rigidbody2D>().isKinematic = true;
-        _activeUnit = null;
+        if (_activeUnit)
+        {
+            _activeUnit.Dragging = false;
+            _activeUnit = null;
+        }      
     }
 }
